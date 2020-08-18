32d04c6c454c69fafb3fc9aedf269f282deae685
Ubuntu <ubuntu@ip-172-31-6-60.ap-northeast-1.compute.internal>
2020-08-18 09:07:35 +0000

    first commit

---
+++ sample_app/app/controllers/users_controller.rb
@@ -0,0 +1,90 @@
+class UsersController < ApplicationController
+  before_action :authenticate_user, {only: [:index, :show, :edit, :update, :logout]}
+  before_action :forbid_login_user, {only: [:new, :create, :login_form, :login]}
+  before_action :ensure_correct_user, {only: [:edit, :update]}
+  
+  
+  def index
+    @users = User.all
+  end
+  
+  def show
+    @user = User.find_by(id: params[:id])
+  end
+  
+  def new
+    @user = User.new
+  end
+  
+  def create
+    @user = User.new(
+      name: params[:name], 
+      email: params[:email], 
+      image_name: "book.jpg",
+      password: params[:password]
+      )
+    if @user.save
+      session[:user_name] = @user.name
+      flash[:notice] = "登録できました"
+      redirect_to("/users/#{@user.id}")
+    else
+      render("users/new")
+    end
+  end
+  
+  def edit
+    @user = User.find_by(id: params[:id])
+  end
+  
+  def update
+    @user = User.find_by(id: params[:id])
+    @user.name = params[:name]
+    @user.email = params[:email]
+    if params[:image]
+      @user.image_name = "#{@user.id}.jpg"
+      image = params[:image]
+      File.binwrite("public/user_images/#{@user.image_name}", image.read)
+    end
+    
+    if @user.save
+      flash[:notice] = "編集できました"
+      redirect_to("/users/#{@user.id}")
+    else 
+      render("users/edit")
+    end
+  end
+  
+  def login_form
+  end
+  
+  def login 
+    @user = User.find_by(email: params[:email])
+    if @user && @user.authenticate(params[:password])
+      session[:user_name] = @user.name
+      flash[:notice] = "ログインしました"
+      redirect_to("/posts/index")
+    else
+      @error_message = "メールアドレスまたはパスワードが間違っています"
+      @email = params[:email]
+      render("users/login_form")
+    end
+  end
+  
+  def logout
+    session[:user_name] = nil
+    flash[:notice] = "ログアウトしました"
+    redirect_to("/login")
+  end
+  
+  def likes
+    @user = User.find_by(id: params[:id])
+    @likes = Like.where(user_id: @user.id)
+  end
+  
+  def ensure_correct_user
+    if @current_user.id != params[:id].to_i
+      flash[:notice] = "権限がありません"
+      redirect_to("/posts/index")
+    end
+  end
+end
